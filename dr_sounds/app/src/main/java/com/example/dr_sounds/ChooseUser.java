a8c3f25db674b2ddc84f7177dc63620476642a33
abelelol <abeleamsalu@gmail.com>
2019-11-14 13:03:24 -0500

    added registration page as well implemented colors into the project

--- dr_sounds/app/src/main/java/com/example/dr_sounds/ChooseUser.java
+++ dr_sounds/app/src/main/java/com/example/dr_sounds/ChooseUser.java
@@ -1,34 +1,35 @@
 package com.example.dr_sounds;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChooseUser extends AppCompatActivity {
-    TextView stuff;
+    TextView new_user;
     ImageButton doctorbutton;
     ImageButton patitentbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.doctor_login);
-
+        setContentView(R.layout.choose_user_type);
+        new_user = (TextView) findViewById(R.id.new_user2);
+        new_user.setPaintFlags(new_user.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
     public void Doctor(View v) {
         Intent intent = new Intent(ChooseUser.this, MainActivity.class);
         startActivity(intent);
-        Log.d("Checker", "this shit should work");
-
     }
 
+    public void newuser(View v){
+        Intent intent = new Intent(ChooseUser.this, Registration.class );
+        startActivity(intent);
+    }
 
 }
---
+++ dr_sounds/app/src/main/java/com/example/dr_sounds/Patient_login.java
@@ -0,0 +1,22 @@
+package com.example.dr_sounds;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Patient_login extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
